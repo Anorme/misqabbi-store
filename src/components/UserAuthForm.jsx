fdb61d1eae6b88f81d3f92e2c6818d29b219ef33
Anorme <anormeinkumsah@gmail.com>
2025-07-14 19:01:40 +0000

    chore(auth): remove placeholder comment after implementing email link authentication

--- src/components/UserAuthForm.jsx
+++ src/components/UserAuthForm.jsx
@@ -143,7 +143,7 @@ const UserAuthForm = () => {
         <span className="mx-4 text-gray-700 font-bold">or</span>
         <hr className="flex-grow border-t-2 border-gray-400" />
       </div>
-      {/* Continue with email for future implementation */}
+
       <button
         className="w-[60%] mx-auto py-3 rounded-md mb-2 flex justify-center items-center gap-2 cursor-pointer"
         type="button"
