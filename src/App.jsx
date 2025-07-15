49a946f76999e9a5ab9a7a91c67805cd181fc513
Anorme <anormeinkumsah@gmail.com>
2025-07-15 17:07:26 +0000

    refactor(auth): switch FinishSignUp component to default export and switch import to reflect that

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import Login from './pages/Login';
 import FinishSignUp from './pages/FinishSignUp';
 
 import './App.css';
-import { FinishSignUp } from './pages/FinishSignUp';
 
 const App = () => {
   return (
